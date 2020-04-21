7edce697cb068c1003561f912998c2c759b42956
MR-LIMs <58262954+MR-LIMs@users.noreply.github.com>
2020-04-21 22:37:30 +0900

    first commit of secondproject with error

---
+++ my_v/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\asd85\onedrive\바탕 화면\like_lion\coding_likelion\second\my_v\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
